81c5e6873de232915807d73433475cb2754a228c
DShilov <abarakus@bk.ru>
2019-02-07 19:06:37 +0300

    Initial commit

---
+++ src/Double.java
@@ -0,0 +1,33 @@
+public class Double {
+    public static double valueOf(String str) throws NumberFormatException {
+        double summ1 = 0;
+        double summ2 = 0;
+        int k1;
+        int k2;
+        if (str.charAt(0) == '-') {
+            k1 = 1;
+            k2 = -1;
+        }
+        else{
+            k1 = 0;
+            k2 = 1;
+        }
+        String[] doubleSplit = str.split("[.,]");
+        for (int i = k1; i < doubleSplit[0].length(); i++) {
+            summ1 += charToInt(doubleSplit[0].charAt(i)) * Math.pow(10, doubleSplit[0].length() - i - 1);
+        }
+        try{
+            for (int i = 0; i < doubleSplit[1].length(); i++) {
+                summ2 += charToInt(doubleSplit[1].charAt(i)) / Math.pow(10, i + 1);
+            }
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("В числе отсутствует дробная часть");
+        }
+        return k2 * (summ1 + summ2);
+    }
+    public static int charToInt(char chr) throws NumberFormatException {
+        if ((int) chr > 48 && (int) chr < 57) {
+            return (int) chr - 48;
+        } else throw new NumberFormatException();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "123.355";
+        System.out.println(Double.valueOf(str));
+    }
+}
