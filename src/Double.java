4236f675f0e6f11ee6dbcbc1fbb20b055551bc76
DShilov <abarakus@bk.ru>
2019-02-12 15:21:11 +0300

    Char conversion

--- src/Double.java
+++ src/Double.java
@@ -1,11 +1,15 @@
 public class Double {
+
     public static double valueOf(String str) throws NumberFormatException {
         return str.charAt(0) == '-' ? convert(str, 1, -1) :  convert(str, 0, 1);
     }
+
     public static double convert(String str, int k1, int k2){
+
         double summ1 = 0;
         double summ2 = 0;
         String[] doubleSplit = str.split("[.,]");
+
         for (int i = k1; i < doubleSplit[0].length(); i++) {
             summ1 += charToInt(doubleSplit[0].charAt(i)) * Math.pow(10, doubleSplit[0].length() - i - 1);
         }
